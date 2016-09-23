b3dc11a78f51e04b97d0305b854fdc2b50779219
kim chang sik <kchs942@naver.com>
2016-09-23 12:35:57 +0900

    B01.java파일

---
+++ B01.java
@@ -0,0 +1,27 @@
+import java.util.*;i
+
+class B01{
+          int age;
+          void input() {
+	  Scanner s=new Scanner(System.in);
+	      
+	   System.out.print("태어난 년도를 입력하시요");
+	          int birth-year=s.nextInt();
+	          this.age=2014-birth-year+1;
+	       print();
+	       }   
+	       
+	    void pirnt(){
+	           if(age<20) 
+		   System.out.println("미성년자 입니다");
+		   else System.out.println("미성년자가 아닙니다");
+
+	   }
+
+          public static void main(Stirngp[ s){
+		        new B01().print();
+
+}
+
+
+}
