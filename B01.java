8ddfe0b084c8b6b6451904261537a32074e72f13
kim chang sik <kchs942@naver.com>
2016-09-23 20:39:46 +0900

    1번문제 완료

--- B01.java
+++ B01.java
@@ -1,4 +1,4 @@
-import java.util.*;i
+import java.util.*;
 
 class B01{
           int age;
@@ -6,20 +6,20 @@ class B01{
 	  Scanner s=new Scanner(System.in);
 	      
 	   System.out.print("태어난 년도를 입력하시요");
-	          int birth-year=s.nextInt();
-	          this.age=2014-birth-year+1;
+	          int birth=s.nextInt();
+	          this.age=2014-birth+1;
 	       print();
 	       }   
 	       
-	    void pirnt(){
+	    void print(){
 	           if(age<20) 
 		   System.out.println("미성년자 입니다");
 		   else System.out.println("미성년자가 아닙니다");
 
 	   }
 
-          public static void main(Stirngp[ s){
-		        new B01().print();
+          public static void main(String[] args){
+		        new B01().input();
 
 }
 
