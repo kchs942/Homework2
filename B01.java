64277aeb359069561f398b50c8103095352f2569
kim chang sik <kchs942@naver.com>
2016-09-23 17:03:13 +0900

    1번과제완성

--- B01.java
+++ B01.java
@@ -1,27 +1,24 @@
-import java.util.*;i
+import java.util.*;
 
 class B01{
-          int age;
-          void input() {
-	  Scanner s=new Scanner(System.in);
-	      
-	   System.out.print("태어난 년도를 입력하시요");
-	          int birth-year=s.nextInt();
-	          this.age=2014-birth-year+1;
-	       print();
-	       }   
-	       
-	    void pirnt(){
-	           if(age<20) 
-		   System.out.println("미성년자 입니다");
-		   else System.out.println("미성년자가 아닙니다");
+         int age;
+        void input(){
+	   Scanner s=new Scanner(System.in);
+	   System.out.print("태어난 년도를 입력하시오");
+	   int birth=s.nextInt();
+           this.age=2014-birth+1;
+	  print();
+	}
 
+	void print(){
+	   if(age<20)
+	   System.out.println("미성년자 입니다");
+	   else System.out.println("미성년자가 아닙니다");
+	
+	}
+           public static void main(String[] args){
+	   
+	   new B01().input();  
+	   
 	   }
-
-          public static void main(Stirngp[ s){
-		        new B01().print();
-
-}
-
-
 }
