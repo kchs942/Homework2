e65a979e1e30f1a887a4592a2948319fb86ad28b
kim chang sik <kchs942@naver.com>
2016-09-23 20:29:27 +0900

    2번문제 완료

--- B02.java
+++ B02.java
@@ -2,25 +2,26 @@ import java.util.*;
 
 class B02{
           String kind;
-	   double in_degree;
-	     double out_degree;
+	  double in_degree;
           void input() {
 	  Scanner s=new Scanner(System.in);
 	      
 	   System.out.print("온도를 입력하세요");
-	          this.in_degree=s.nextDouble();
+	           
+		   this.in_degree=s.nextDouble();
 	          System.out.println("섭씨온도 이면c,화씨온도이면f");
 		  this.kind=s.next();
 	        print();
 	       }   
 	       
 	    void print(){
-	           if(this.kind=="c"){
-                   this.out_degree=this.in_degree*1.8+32;
-		   System.out.println("변환된 온도는"+this.out_degree);
+	           if(this.kind.equals("c")){
+                   double out_degree=(this.in_degree*1.8)+32;
+		   System.out.println("변환된 온도는"+out_degree);
 		   }
-		   if(this.kind=="f"){
-		    this.out_degree=(this.in_degree-32)/1.8;		                       System.out.println("변환된 온도는"+this.out_degree);
+		   if(this.kind.equals("f")){
+		   double out_degree=(this.in_degree-32)/1.8;
+		    System.out.println("변환된 온도는"+out_degree);
 		    }
 	   }
 
